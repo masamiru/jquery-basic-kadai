8998e049d7ceaf6c9d2d757bb46e223568c38055
masamiru <162574177+masamiru@users.noreply.github.com>
2024-04-27 08:20:34 +0900

    Add files via upload

---
+++ kadai_004/event.js
@@ -0,0 +1,14 @@
+
+$(function() {
+
+    $(window).on('load', () => {
+        console.log('loadイベントが発生しました');
+    });
+    
+
+    $(window).on('scroll', () => {
+      console.log('scrollイベントが発生しました');
+
+     
+    });
+  });
